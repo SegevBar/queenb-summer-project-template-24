08242ee907ead3e16f246825c5de43e3c548a31c
Noa Gelbart <gelbartn@post.bgu.ac.il>
2024-09-10 23:53:54 +0300

    Building the home page

---
+++ client/src/components/common/FilterBar/FilterBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './FilterBar.module.css';
+
+
+
+
+const FilterBar = () => {
+    return (
+        <span className={styles.filterBar}>
+        </span>
+    )
+};
+
+export default FilterBar;
