94b6ca79fb921e4606e72f50b7bbf031e2ddf99c
Omer Kuriel <omer.kuriel@gmail.com>
2024-09-19 12:01:37 +0300

    continued with the feature development

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,36 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/HomePage/HomePage';
 import styles from './styles/App.module.css';
+import Login from './pages/login'
+import { useLogout } from './hooks/useLogout';
+import Signup from './pages/signup'
+
 
 function App() {
+  const { logout } = useLogout()
+
+  const handleClick = () => {
+    logout()
+  }
+
   return (
     <BrowserRouter>
       <div className={styles.app}>
         <header className={styles.appHeader}>
           <img src="/project-logo.png" alt="Logo" className={styles.appLogo} />
           <nav className={styles.appNav}>
-            <Link to="/" className={styles.appLink}>Log in</Link>
-            <Link to="/" className={styles.appLink}>Sign up</Link>
+            <Link to="/login" className={styles.appLink}>Log in</Link>
+            <Link to="/signup" className={styles.appLink}>Sign up</Link>
+            <div>
+              <button onclick={handleClick}>Log out</button>
+            </div>
           </nav>
         </header>
         <main className={styles.main}>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </main>
         <footer className={styles.footer}>
